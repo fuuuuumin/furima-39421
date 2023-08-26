c0778880950eded983003859e6f8ad8c1a843c52
fuuuuumin <tmfha3@gmail.com>
2023-08-26 17:29:44 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
   belongs_to :shipment
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "¥300~¥9,999,999で指定してください" }, format:{ with: /\A[0-9]+\z/, message: '半角数字で入力して下さい。' }
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :postage_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipment_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
 end
