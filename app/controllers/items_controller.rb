9d7b2817a2f0a1a8cf0f4baff23b3774d6b3d995
fuuuuumin <tmfha3@gmail.com>
2023-08-28 20:47:59 +0900

    商品が出品できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_id, :image).merge(user_id: current_user.id)
+  end
 end
